aa7f3ef41c2e9b9425cb7fdf6ec3e6024dc4ad68
suresh <kadamsuresh37697@gmail.com>
2023-02-02 14:34:33 +0530

    Selection Sort Updated

--- BubbleSortDemo.java
+++ BubbleSortDemo.java
@@ -1,21 +1,22 @@
-class BubbleSortDemo {
+class SelectionSortDemo {
 
 
-static void bsort(int arr[])
+static void ssort(int arr[])
 {   
     int n=arr.length;
     for(int i=0;i<n-1;i++)
     { 
-        for(int j=0;j<n-1-i;j++)
+        int min=i;
+        for(int j=i+1;j<n;j++)
         {
-             if(arr[j]>arr[j+1])
-             { 
-                int temp;
-              temp=arr[j];
-              arr[j]=arr[j+1];
-              arr[j+1]=temp;
+             if(arr[j]<arr[min])
+             min=j;
                
              }
+   int temp=arr[min];
+   arr[min]=arr[i];
+   arr[i]=temp;
+
              display(arr);
             System.out.println();
 
@@ -42,7 +43,7 @@ static void display(int arr[])
 public static void main (String args[])
 {
   int arr[]={54,12,89,55,47};
-  bsort(arr);
+  ssort(arr);
 
 }
 
